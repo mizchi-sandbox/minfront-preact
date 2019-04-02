39260fcd6d2ce824f23b5618752eb1ba3d8a5466
mizchi <miz404@gmail.com>
2019-04-03 07:23:05 +0900

    Add preact

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,16 @@
+import { h, render } from 'preact';
+import { useState, useCallback } from 'preact/hooks'
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = useCallback(() => setCounter(counter => counter + 1), []);
+  return <div>
+    <h1>Hello Preact</h1>
+    <div>
+      <button onClick={onClick}>{counter}</button>
+    </div>
+  </div>
+}
+
 const root = document.querySelector(".root") as HTMLDivElement;
-root.textContent = "Hello, {app_name}";
+render(<App />, root);
